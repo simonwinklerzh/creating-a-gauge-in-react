ba160f5f7759310c0b2be18f39ec467aeaa1f63e
Simon Winkler <simonwinkler@Simons-MacBook-Pro.local>
2020-06-14 23:31:24 +0200

    Create initial gauge component with the desired viewbox

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Gauge = ({
+  value = 50,
+  min = 0,
+  max = 100,
+  label,
+  units
+}) => {
+  return (
+    <div>
+      <svg
+        width="9em"
+        viewBox={[
+          -1, // x
+          -1, // y
+          2,  // width
+          1   // height
+        ]}
+        style={{
+          border: "1px solid pink"
+        }}>
+      </svg>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <Gauge />
   </React.StrictMode>,
   document.getElementById('root')
 );
