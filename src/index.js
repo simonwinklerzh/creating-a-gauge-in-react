25a445ee5eb0c775a3e0fd426e1c6548f1db47aa
Simon Winkler <simonwinkler@Simons-MacBook-Pro.local>
2020-06-15 09:06:02 +0200

    Draw a background arc and a value arc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { arc } from 'd3-shape';
 import './index.css';
 
 const Gauge = ({
@@ -9,6 +10,25 @@ const Gauge = ({
   label,
   units
 }) => {
+  const backgroundArc = arc()
+    .innerRadius(0.65)
+    .outerRadius(1)
+    .startAngle(-Math.PI / 2)
+    .endAngle(Math.PI / 2)
+    .cornerRadius(1);
+
+  // Note: This is the solution I came up with.
+  // The tutorial uses the `scaleLinear` function from d3 to calculate the angle.
+  const valueEndAngle =
+    (-Math.PI / 2) + (Math.PI / (max - min) * value);
+
+  const valueArc = arc()
+    .innerRadius(0.65)
+    .outerRadius(1)
+    .startAngle(-Math.PI / 2)
+    .endAngle(valueEndAngle)
+    .cornerRadius(1);
+
   return (
     <div>
       <svg
@@ -17,11 +37,19 @@ const Gauge = ({
           -1, // x
           -1, // y
           2,  // width
-          1   // height
-        ]}
+          2   // height
+        ].join(' ')}
         style={{
           border: "1px solid pink"
         }}>
+        <path
+          d={backgroundArc()}
+          fill="#dbdbe7">
+        </path>
+        <path
+          d={valueArc()}
+          fill="#ffdbe7">
+        </path>
       </svg>
     </div>
   );
