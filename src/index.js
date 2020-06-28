487faf8b9fa49f19a15f93897cc7ceecda4a7e05
Simon Winkler <simon.winkler@viu.ch>
2020-06-28 21:10:13 +0200

    Add rc-slider and connect the slider component with the gauge component so that the slider can controll the gauge

--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Slider from 'rc-slider';
 import { Gauge } from './components/gauge/gauge';
+import 'rc-slider/assets/index.css';
 import './index.css';
+
+const GaugeWithSlider = ({
+  value = 60,
+  min = 0,
+  max = 200,
+  label = "Speed",
+  units = "kilometers per hour"
+} = {}) => {
+  const [ valueState, setValuestate ] = useState(value);
+
+  return (
+    <div className="gauge-container">
+      <Gauge {...{
+        value: valueState,
+        min,
+        max,
+        label,
+        units
+      }} />
+      <Slider {...{
+        value: valueState,
+        min,
+        max,
+        marks: {[min]: min, [max]: max},
+        onChange: setValuestate
+      }} />
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <div style={{marginBottom: '1em'}}>
-      <Gauge
-        value="100"
-        label="Atmospheric Pressure"
-        units="hectopascals" />
-    </div>
-    <div style={{marginBottom: '1em'}}>
-      <Gauge
-        value={40}
-        label="Wind speed"
-        units="meters per second" />
-    </div>
-    <div style={{marginBottom: '1em'}}>
-      <Gauge
-        label="Kilometers driven"
-        value={70}
-        units="kilometers" />
-    </div>
+    <GaugeWithSlider
+      value={0}
+      max={2000}
+      label="Atmospheric Pressure"
+      units="hectopascals" />
+    <GaugeWithSlider
+      value={40}
+      label="Wind speed"
+      units="meters per second" />
+    <GaugeWithSlider
+      label="Kilometers driven"
+      value={70}
+      units="kilometers" />
   </React.StrictMode>,
   document.getElementById('root')
 );
