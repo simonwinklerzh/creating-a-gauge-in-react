373e1d569e2b4366981efa71896e3016d2f9192b
Simon Winkler <simon.winkler@viu.ch>
2020-06-29 17:41:53 +0200

    Use localstorage to persist state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Slider from 'rc-slider';
 import { Gauge } from './components/gauge/gauge';
 import 'rc-slider/assets/index.css';
 import './index.css';
 
+// Hook
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = value => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
 const GaugeWithSlider = ({
   value = 60,
   min = 0,
   max = 200,
   label = "Speed",
-  units = "kilometers per hour"
+  units = "kilometers per hour",
+  componentId // Provide a component ID if you want to persist the state to local storage
 } = {}) => {
-  const [ valueState, setValuestate ] = useState(value);
+  const [ valueState, setValuestate ] = useLocalStorage(`gauge_control_center_state_${componentId}`, value);
 
   return (
     <div className="gauge-container">
@@ -43,11 +81,23 @@ ReactDOM.render(
       value={0}
       max={2000}
       label="Atmospheric Pressure"
-      units="hectopascals" />
+      units="hectopascals"
+      componentId="1" />
     <GaugeWithSlider
       value={40}
       label="Wind speed"
-      units="meters per second" />
+      units="meters per second"
+      componentId="2" />
+    <GaugeWithSlider
+      label="Kilometers driven"
+      value={70}
+      units="kilometers"
+      componentId="3" />
+    <GaugeWithSlider
+      label="Kilometers driven"
+      value={70}
+      units="kilometers"
+      componentId="3" />
     <GaugeWithSlider
       label="Kilometers driven"
       value={70}
